7a35c4f25a457ca65f0b730a98bb5164941317ad
Guan Yu Pan (Jacky) <jackypan1989@gmail.com>
2017-11-07 17:59:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ hexo.extend.helper.register('list_authors', function() {
     const count_posts = author => this.site.posts.filter(post => post.author === author).length;
     const authors = this.site.authors.map(author => `
         <li class="author-list-item">
-            <a class="author-list-link" href="${author_to_url.call(this, author)}">${author}</a>
+            <a class="author-list-link" href="${author_to_url.call(this, author)/}">${author}</a>
             <span class="author-list-count">${count_posts(author)}</span>
         </li>`).join('');
 
